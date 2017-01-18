ec365fce76060d1d273bb1b80ce000db36f443cc
Jordan Teachworth <jmteachw@gmail.com>
2017-01-18 01:12:18 -0600

    Tied the RSVP form into the wedding-api call, so now RSVP is working

--- js/index.js
+++ js/index.js
@@ -17,3 +17,45 @@ $(function() {
         event.preventDefault();
     });
 });
+
+/* RSVP submit fucntion */
+
+// Attach a submit handler to the form
+$("#rsvpForm").submit(function(event) {
+ 
+	// Stop form from submitting normally
+	event.preventDefault();
+
+	// Get some values from elements on the page:
+	var $form = $(this),
+	firstName = $form.find("input[name='first-name']").val(),
+	lastName = $form.find("input[name='last-name']").val(),
+	email = $form.find("input[name='email']").val(),
+	attending = $form.find("input[name='attending']:checked").val();
+
+	// Send the data using post
+	var posting = $.post("http://api.alyandjordan.us:8080/api/rsvp/", { "first_name": firstName, "last_name": lastName, "email": email, "attending": attending === 'true'});
+
+	// Vars for the modal
+	var alertClass = 'alert-success';
+	var message = 'Success! Thank you for RSVP\'ing!';
+
+	// If the call was a failure
+	posting.fail(function(response) {
+		if(response.responseText == '{"non_field_errors":["The fields first_name, last_name must make a unique set."]}') {
+			alertClass = 'alert-info';
+			message = 'You\'re already RSVP\'d!';
+		}
+		else {
+			alertClass = 'alert-danger';
+        	message = 'Something went wrong! Please try again later.';
+    	}
+	});
+
+	// Show the message modal based on result
+	posting.always(function() {
+		$('#rsvpMessages').removeClass('hide').addClass('alert ' + alertClass + ' alert-dismissible').slideDown().show();
+        	$('#rsvpMessagesContent').html('<h4>' + message + '</h4>');
+        	$('#modal').modal('show');
+	});
+});
